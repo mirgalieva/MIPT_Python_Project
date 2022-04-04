f5d21083dd75243859fae5802f6ef30b54ed795d
mirgalieva <mirgalieva.ai@phystech.edu>
2022-04-04 23:53:54 +0300

    first review

--- Ciphers/caesarCipher.py
+++ Ciphers/caesarCipher.py
@@ -9,27 +9,29 @@ class CaesarCipher:
     def shiftedForwardText(cls, shift, text):
         result = str()
         for symbol in text:
-            if (symbol.isupper()):
-                result += chr((ord(symbol) + shift - 65) % 26 + 65)
-                # Encrypt lowercase characters in plain text
+            if symbol.isalpha():
+                if symbol.isupper():
+                    result += chr((ord(symbol) + shift - 65) % 26 + 65)
+                else:
+                    result += chr((ord(symbol) + shift - 97) % 26 + 97)
             else:
-                result += chr((ord(symbol) + shift - 97) % 26 + 97)
+                result += symbol
         return result
 
     @classmethod
     def shiftedBackardText(cls, shift, text):
-        for key in range(len(LETTERS)):
-            translated = ''
-            for symbol in message:
-                if symbol in LETTERS:
-                    num = LETTERS.find(symbol)
+        for key in range(len(cls.alphabet)):
+            decoded = ''
+            for symbol in text:
+                if symbol.isalpha:
+                    num = cls.alphabet.find(symbol)
                     num = num - key
                     if num < 0:
-                        num = num + len(LETTERS)
-                    translated = translated + LETTERS[num]
+                        num = num + 26
+                    decoded = decoded + cls.alphabet[num]
                 else:
-                    translated = translated + symbol
-        print('Hacking key #%s: %s' % (key, translated))
+                    decoded = decoded + symbol
+        return decoded
 
     @classmethod
     def decode(cls, arguments):
@@ -45,15 +47,16 @@ class CaesarCipher:
         for alpha in cls.alphabet:
             dict[alpha.lower()] = 0
         for alpha in text:
-            dict[alpha.lower] += 1
-        for value in dict:
+            if alpha.isalpha():
+                dict[alpha.lower()] += 1
+        for value in dict.values():
             value /= len(text)
         return dict
     @classmethod
     def errorFunction(cls, frequencys):
         sum = 0
-        for alpha in cls.alphabet:
-            sum += (frequencys[alpha] - cls.english_frequences[alpha])**2
+        for i in range(len(cls.alphabet)):
+            sum += (frequencys[cls.alphabet[i]] - cls.english_frequences[i])**2
         sum /= len(cls.alphabet)
         return sum**0.5
     @classmethod
@@ -62,7 +65,7 @@ class CaesarCipher:
         shifted_text = cls.shiftedForwardText(0,arguments["input_text"])
         frequencys = cls.getFrequencys(shifted_text)
         bestError = cls.errorFunction(frequencys)
-        for shift in range(-30,30):
+        for shift in range(-26,26):
             shifted_text = cls.shiftedForwardText(shift, arguments["input_text"])
             frequencys = cls.getFrequencys(shifted_text)
             error = cls.errorFunction(frequencys)
